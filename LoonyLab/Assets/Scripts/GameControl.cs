2995e874954be60f2b284393e747efd624fab58a
Jessica Glustien <jglustien@gmail.com>
2019-05-29 00:15:38 -0400

    fixed some stuff

--- LoonyLab/Assets/Scripts/GameControl.cs
+++ LoonyLab/Assets/Scripts/GameControl.cs
@@ -10,6 +10,7 @@ public class GameControl : MonoBehaviour
     public GameObject leftHand;
     public GameObject rightHand;
     public GameObject balancingScreen;
+    public GameObject customer1;
 
     public Text chem1;
     public Text chem2;
@@ -38,6 +39,9 @@ public class GameControl : MonoBehaviour
             {
                 balancing = false;
                 balancingScreen.SetActive(false);
+                rightHand.SetActive(true);
+                SpriteRenderer sr = rightHand.GetComponent<SpriteRenderer>();
+                sr.color = Color.magenta;
             }
         } 
     }
@@ -75,8 +79,9 @@ public class GameControl : MonoBehaviour
     {
         float player_x = player.transform.position.x;
         float player_y = player.transform.position.y;
+        if (player_y < threshold && 6.5 < player_x && leftHand.activeSelf && rightHand.activeSelf)
+        {
 
-      
             balancingScreen.SetActive(true);
             leftHand.SetActive(false);
             rightHand.SetActive(false);
@@ -87,9 +92,22 @@ public class GameControl : MonoBehaviour
             chem2.text = "Cl2";
             result.text = "FeCl3";
 
-
+        }
 
         
+    }
+
+    public void CustomerClick()
+    {
+        float player_x = player.transform.position.x;
+        float player_y = player.transform.position.y;
+
+        if (!leftHand.activeSelf && rightHand.activeSelf && player_x < -5.5 && player_y > 1.5)
+        {
+            rightHand.SetActive(false);
+            customer1.SetActive(false);
+        }
+
     }
 
 }
