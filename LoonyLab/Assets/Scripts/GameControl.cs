4ab7a8caaec78b66dae2fd2a33a6987c32fc63a2
Jessica Glustien <jglustien@gmail.com>
2019-06-09 17:14:54 -0400

    updated game mechanics

--- LoonyLab/Assets/Scripts/GameControl.cs
+++ LoonyLab/Assets/Scripts/GameControl.cs
@@ -7,8 +7,7 @@ using System;
 public class GameControl : MonoBehaviour
 {
     public GameObject player;
-    public GameObject leftHand;
-    public GameObject rightHand;
+    public GameObject Hand;
     public GameObject balancingScreen;
     public GameObject customer1;
     public GameObject customer2;
@@ -16,6 +15,10 @@ public class GameControl : MonoBehaviour
     public Text chem1;
     public Text chem2;
     public Text result;
+
+    public Text chem1Total;
+    public Text chem2Total;
+    public Text resultTotal;
     public Text customer1Text;
     public Text customer2Text;
 
@@ -24,44 +27,42 @@ public class GameControl : MonoBehaviour
     public Text fix4;
     public Text fix5;
 
-    public InputField inChem1;
-    public InputField inChem2;
-    public InputField inResult;
-
     public int threshold;
-    private Dictionary<string, Color> chemicals = new Dictionary<string, Color>();
-    private Dictionary<Tuple<string, string>, Tuple<string, int, int, int, Color>> results = new Dictionary<Tuple<string, string>, Tuple<string, int, int, int, Color>>();
-    private string leftName = "X";
-    private string rightName = "X";
 
-    private bool balancing = false;
+    private Chemical InHand; // Chemical player is currently holding. 
 
-    private char sub_2 = (char)8322;
-    private char sub_3 = (char)8323;
-    private List<string> chemNames = new List<string>();
-    private List<string> orders = new List<string>();
+
+    private char sub_2 = (char)8322; // Subscript 2
+    private char sub_3 = (char)8323; // Subscript 3
+
+
+    private List<Chemical> chemicals = new List<Chemical>(); // List of chemicals player can pick up. 
+    private List<string> orders = new List<string>(); // List of orders customers will make throughout level. 
+
+    private Dictionary<Tuple<Chemical, Chemical>, Chemical> results = new Dictionary<Tuple<Chemical, Chemical>, Chemical>();
+
+    private BalancingStation balanceStn = new BalancingStation();
 
     // Start is called before the first frame update
     void Start()
     {
+        Chemical fe = new Chemical("Fe", 1, 0, false, Color.blue, "Fe");
+        Chemical cl2 = new Chemical("Cl" + sub_2, 2, 0, false, Color.red, "Cl");
+        Chemical o2 = new Chemical("O" + sub_2, 2, 0, false, Color.green, "O");
+        Chemical h2 = new Chemical("H" + sub_2, 2, 0, false, Color.magenta, "H");
 
-        chemicals["Fe"] = Color.blue;
-        chemicals["Cl" + sub_2] = Color.red;
-        chemicals["O" + sub_2] = Color.green;
-        chemicals["H" + sub_2] = Color.gray;
-
-        results[Tuple.Create("Fe", "Cl" + sub_2)] = Tuple.Create("FeCl" + sub_3, 2, 3, 2, Color.magenta);
-        results[Tuple.Create("Cl" + sub_2, "Fe")] = Tuple.Create("FeCl" + sub_3, 3, 2, 2, Color.magenta);
-        results[Tuple.Create("Fe", "O" + sub_2)] = Tuple.Create("Fe" + sub_2 +"O" + sub_3, 4, 3, 2, Color.cyan);
-        results[Tuple.Create("O" + sub_2, "Fe")] = Tuple.Create("Fe" + sub_2 + "O" + sub_3, 3, 4, 2, Color.cyan);
-        results[Tuple.Create("O" + sub_2, "H" + sub_2)] = Tuple.Create("H" + sub_2 + "O", 1, 2, 2, Color.black);
-        results[Tuple.Create("H" + sub_2, "O" + sub_2)] = Tuple.Create("H" + sub_2 + "O", 2, 1, 2, Color.black);
+        results[Tuple.Create(fe, cl2)] = new Chemical("FeCl" + sub_3, 1, 3, true, Color.yellow, "FeCl");
+        results[Tuple.Create(cl2, fe)] = new Chemical("FeCl" + sub_3, 3, 1, true, Color.yellow, "FeCl");
+        results[Tuple.Create(fe, o2)] = new Chemical("Fe" + sub_2 + "O" + sub_3, 2, 3, true, Color.cyan, "FeO");
+        results[Tuple.Create(o2, fe)] = new Chemical("Fe" + sub_2 + "O" + sub_3, 3, 2, true, Color.cyan, "FeO");
+        results[Tuple.Create(o2, h2)] = new Chemical("H" + sub_2 + "O", 2, 1, true, Color.black, "HO");
+        results[Tuple.Create(h2, o2)] = new Chemical("H" + sub_2 + "O", 1, 2, true, Color.black, "HO");
 
 
-        chemNames.Add("Fe");
-        chemNames.Add("Cl" + sub_2);
-        chemNames.Add("O" + sub_2);
-        chemNames.Add("H" + sub_2);
+        chemicals.Add(fe);
+        chemicals.Add(cl2);
+        chemicals.Add(o2);
+        chemicals.Add(h2);
 
         orders.Add("FeCl" + sub_3);
         orders.Add("Fe" + sub_2 + "O" + sub_3);
@@ -81,22 +82,7 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if (balancing)
-        {
-            if (CheckCorrect())
-            {
-                var tpl = Tuple.Create(leftName, rightName);
-                balancing = false;
-                balancingScreen.SetActive(false);
-                rightHand.SetActive(true);
-                rightName = results[tpl].Item1;
-                SpriteRenderer sr = rightHand.GetComponent<SpriteRenderer>();
-                sr.color = results[tpl].Item5;
-                inChem1.text = "";
-                inChem2.text = "";
-                inResult.text = "";
-            }
-        } 
+
     }
 
     public void GenerateCustomers()
@@ -121,58 +107,28 @@ public class GameControl : MonoBehaviour
 
     public void ChemicalClick(int chemNum)
     {
-        float player_x = player.transform.position.x;
-        float player_y = player.transform.position.y;
-        string chemName = chemNames[chemNum];
-
-        if (Math.Abs(player_y - 250) < threshold && player_x < 0)
-        {
-            if (!rightHand.activeSelf)
+        Chemical chem = chemicals[chemNum];
+            if (!Hand.activeSelf)
             {
-                rightHand.SetActive(true);
-                SpriteRenderer sr = rightHand.GetComponent<SpriteRenderer>();
-                rightName = chemName;
-                sr.color = chemicals[chemName];
+                Hand.SetActive(true);
+                SpriteRenderer sr = Hand.GetComponent<SpriteRenderer>();
+                InHand = chem;
+                sr.color = InHand.Colour;
             }
-            else if (!leftHand.activeSelf)
-            {
-                leftHand.SetActive(true);
-                SpriteRenderer sr = leftHand.GetComponent<SpriteRenderer>();
-                leftName = chemName;
-                sr.color = chemicals[chemName];
-            }
-        }
+        
     }
 
-    public bool CheckCorrect()
-    {
-        string a = inChem1.text;
-        string b = inChem2.text;
-        string c = inResult.text;
 
-
-        var tpl = Tuple.Create(leftName, rightName);
-
-        return (a == results[tpl].Item2.ToString() && b == results[tpl].Item3.ToString() && c == results[tpl].Item4.ToString());
-
-    }
-
-    public void TrashClick(bool right)
+    public void TrashClick()
     {
         float player_x = player.transform.position.x;
         float player_y = player.transform.position.y;
 
         if (Math.Abs(player_y + 250) < threshold)
         {
-            if (right && Math.Abs(player_x + 100) < threshold)
+            if (Math.Abs(player_x + 100) < threshold)
             {
-                rightHand.SetActive(false);
-                rightName = "X";
-            }
-            else if (!right && Math.Abs(player_x - 70) < threshold)
-            {
-                leftHand.SetActive(false);
-                leftName = "X";
+                Hand.SetActive(false);
             }
         }
     }
@@ -182,30 +138,94 @@ public class GameControl : MonoBehaviour
         float player_x = player.transform.position.x;
         float player_y = player.transform.position.y;
 
-        if (player_y < threshold && 6.5 < player_x && leftHand.activeSelf && rightHand.activeSelf)
+        if (player_y < threshold && 6.5 < player_x && Hand.activeSelf)
         {
-            if (CheckChemicals())
+
+            balancingScreen.SetActive(true);
+            Hand.SetActive(false);
+            if (balanceStn.Reactant1 == null)
             {
-                balancingScreen.SetActive(true);
-                leftHand.SetActive(false);
-                rightHand.SetActive(false);
+                balanceStn.Reactant1 = InHand;
+                balanceStn.QuantityR1 = 1;
+                chem1.text = "1  " + InHand.Name;
+                chem1Total.text = "Total: " + InHand.Subscript1.ToString() + " " + InHand.SingleName + " Molecules";
 
-                balancing = true;
-
-                var tpl = Tuple.Create(leftName, rightName);
-                chem1.text = leftName;
-                chem2.text = rightName;
-                result.text = results[tpl].Item1;
             }
+            else
+            {
+                if (balanceStn.Reactant1 == InHand)
+                {
+                    balanceStn.QuantityR1++;
+                    chem1.text = balanceStn.QuantityR1.ToString() + " " + InHand.Name;
+                    chem1Total.text = "Total: " + (balanceStn.Reactant1.Subscript1 * balanceStn.QuantityR1).ToString() + " " + InHand.SingleName + " Molecules";
+                    UpdateBalanced();
+                }
+                else
+                {
+                    if (balanceStn.Reactant2 == null)
+                    {
+                        balanceStn.Reactant2 = InHand;
+                        balanceStn.QuantityR2 = 1;
+                        chem2.text = "1  " + InHand.Name;
+                        chem2Total.text = "Total: " + InHand.Subscript1 + " " + InHand.SingleName + " Molecules";
 
+                        if (results.ContainsKey(Tuple.Create(balanceStn.Reactant1, balanceStn.Reactant2)))
+                        {
+                            balanceStn.Product = results[Tuple.Create(balanceStn.Reactant1, balanceStn.Reactant2)];
+                            UpdateBalanced();
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (balanceStn.Reactant2 == InHand)
+                        {
+                            balanceStn.QuantityR2++;
+                            chem2.text = balanceStn.QuantityR2.ToString() + " " + InHand.Name;
+                            chem2Total.text = "Total: " + (InHand.Subscript1 * balanceStn.QuantityR2).ToString() + " " + InHand.SingleName + " Molecules";
+                            UpdateBalanced();
+                        }
+                        else
+                        {
+                            balancingScreen.SetActive(false);
+                            Hand.SetActive(true);
+                        }
+                    }
+                }
+            }
         }
 
     }
 
-    public bool CheckChemicals()
+    public void UpdateBalanced()
     {
-        return results.ContainsKey(Tuple.Create(leftName, rightName));
+
+        int num1 = (int)Math.Ceiling(balanceStn.QuantityR1 * balanceStn.Reactant1.Subscript1 / (float)balanceStn.Product.Subscript1);
+        int num2 = (int)Math.Ceiling(balanceStn.QuantityR2 * balanceStn.Reactant2.Subscript1 / (float)balanceStn.Product.Subscript2);
+        int goal = Math.Max(num1, num2);
+        result.text = goal.ToString() + " " + balanceStn.Product.Name;
+        int missing1 = balanceStn.Product.Subscript1 * goal - balanceStn.QuantityR1 * balanceStn.Reactant1.Subscript1;
+        int missing2 = balanceStn.Product.Subscript2 * goal - balanceStn.QuantityR2 * balanceStn.Reactant2.Subscript1;
+
+        if (missing1 == 0 && missing2 == 0)
+        {
+            // Balanced 
+
+            resultTotal.text = "Balanced! You have finished the reaction for creating " + balanceStn.Product.Name + "!";
+            Hand.SetActive(true);
+            InHand = balanceStn.Product;
+
+            SpriteRenderer sr = Hand.GetComponent<SpriteRenderer>();
+            sr.color = InHand.Colour;
+        }
+        else
+        {
+            // Not balanced
+
+            resultTotal.text = "Missing " + missing1.ToString() + " " + balanceStn.Reactant1.SingleName + " and " + missing2.ToString() + " " + balanceStn.Reactant2.SingleName;
+        }
     }
+
 
     public void CustomerClick(int num)
     {
@@ -223,18 +243,76 @@ public class GameControl : MonoBehaviour
 
         if (player_x < -5.5 && player_y > -1)
         {
-            if (order == rightName)
+            if (order == InHand.Name)
             {
                 if (num == 0)
                     customer1.SetActive(false);
                 else if (num == 1)
                     customer2.SetActive(false);
-                rightHand.SetActive(false);
+                Hand.SetActive(false);
 
             }
         }
 
     }
 
+    public void CloseScreen()
+    {
+        balancingScreen.SetActive(false);
+        if (Hand.activeSelf)
+            ClearScreen();
+    }
+
+    public void ClearScreen()
+    {
+        balanceStn.Reactant1 = null;
+        balanceStn.Reactant2 = null;
+        balanceStn.QuantityR1 = 0;
+        balanceStn.QuantityR2 = 0;
+        balanceStn.Product = null;
+
+        chem1.text = "";
+        chem2.text = "";
+        result.text = "";
+        chem1Total.text = "";
+        chem2Total.text = "";
+        resultTotal.text = "";
+    }
+
 }
+
+
+
+public class Chemical
+{
+    public string Name;
+    public string SingleName;
+    public int Subscript1;
+    public int Subscript2;
+    public bool Product;
+    public Color Colour;
+
+    public Chemical(string name, int sub1, int sub2, bool product, Color colour, string singleName)
+    {
+        Name = name;
+        Subscript1 = sub1;
+        Subscript2 = sub2;
+        Product = product;
+        Colour = colour;
+        SingleName = singleName;
+    }
+}
+
+public class BalancingStation
+{
+    public Chemical Reactant1;
+    public Chemical Reactant2;
+
+    public int QuantityR1;
+    public int QuantityR2;
+
+    public Chemical Product;
+}
+
+
 
