fdcd8409216223516c72997f5108033efc450ee8
perrinni <nick.perrin@mail.utoronto.ca>
2019-06-24 22:41:20 -0400

    Player movement animation added, target changed to win

--- LoonyLab/Assets/Scripts/PlayerController.cs
+++ LoonyLab/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
     public bool balancing = false;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,35 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput * speed;
+
+        // Set 4-way direction in this particular order to prioritize sideways walking animation
+
+        animator.SetBool("movingUp", false);
+        animator.SetBool("movingDown", false);
+        animator.SetBool("movingRight", false);
+        animator.SetBool("movingLeft", false);
+
+        if (moveVelocity[1] > 0) {
+            animator.SetBool("movingUp", true);
+        }
+        else if (moveVelocity[1] < 0) {
+            animator.SetBool("movingDown", true);
+        }
+        else {
+            animator.SetBool("movingUp", false);
+            animator.SetBool("movingDown", false);
+        }
+
+        if (moveVelocity[0] > 0) {
+            animator.SetBool("movingRight", true);
+        }
+        else if (moveVelocity[0] < 0) {
+            animator.SetBool("movingLeft", true);
+        }
+        else {
+            animator.SetBool("movingRight", false);
+            animator.SetBool("movingLeft", false);
+        }
     }
 
     private void FixedUpdate()
